f2e468e17b7de985ad56ebbd56f8e262174b0cc0
Victor Mu <vic.mgs@gmail.com>
2016-12-27 14:47:53 -0800

    Implement insertion sort

---
+++ selectionSort.js
@@ -0,0 +1,19 @@
+selectionSort = function(array) {
+  for (var i = 0; i < array.length; i++) {
+    var minIndex = i;
+    for (var j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      var temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
+    }
+  }
+  return array;
+}
+
+console.log(selectionSort([1,6,3,5,7,8,2]));
+console.log(selectionSort(['z', 'a', 't', 'c', 'f', 'v']));
