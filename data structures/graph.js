99487a9e5361c9cda60f8565f16a66057a285760
Victor Mu <vic.mgs@gmail.com>
2016-12-31 16:15:20 -0800

    Implement depth first methods

--- data structures/graph.js
+++ data structures/graph.js
@@ -14,6 +14,58 @@ Graph.prototype.addEdge = function(v, w) {
   this.edges++;
 }
 
+Graph.prototype.depthFirstSearch = function(vertex) {
+  var marked = new Array(this.vertices.length);
+  var graph = this;
+  var dfs = function(vertex) {
+    marked[vertex] = true;
+    graph.adj(vertex).forEach(function(vertex) {
+      if (!marked[vertex]) {
+        dfs(vertex);
+      }
+    })
+  }
+  dfs(vertex);
+  return marked;
+}
+
+Graph.prototype.depthFirstPaths = function(vertex) {
+  var marked = new Array(this.vertices.length);
+  var pathTo = new Array(this.vertices.length);
+  pathTo[vertex] = vertex;
+  var graph = this;
+  var dfp = function(vertex) {
+    marked[vertex] = true;
+    graph.adj(vertex).forEach(function(connect) {
+      if (!marked[connect]) {
+        pathTo[connect] = vertex;
+        dfp(connect);
+      }
+    })
+  }
+  dfp(vertex);
+
+  return pathTo;
+}
+
+Graph.prototype.pathTo = function(vertex) {
+  var pathTo = this.depthFirstPaths(0);
+  var paths = [];
+  var path = ''+vertex;
+  for (var i = 0; i < this.vertices.length; i++) {
+    paths.push(null);
+  }
+  paths[vertex] = path.split('');
+
+  while (pathTo[vertex] !== vertex) {
+    vertex = pathTo[vertex];
+    path = vertex + path;
+    paths[vertex] = path.split('');
+  }
+
+  return paths;
+}
+
 Graph.prototype.toString = function() {
   for (var i = 0; i < this.vertices.length; i++) {
     var edges = [];
@@ -24,13 +76,21 @@ Graph.prototype.toString = function() {
   }
 }
 
-// var adjacent = function(vertex) {
-//   for(var node = vertex.first; node; node = node.next) {
-//
-//   }
-// }
+Graph.prototype.adj = function(vertex) {
+  var connects = [];
+  for(var node = this.vertices[vertex].first; node; node = node.next) {
+    connects.push(node.value);
+  }
+  return connects;
+}
 
-test = new Graph(10);
-test.addEdge(1,3);
-test.addEdge(1,5);
-// test.toString();
+test = new Graph(6);
+test.addEdge(2,4);
+test.addEdge(2,3);
+test.addEdge(1,2);
+test.addEdge(0,5);
+test.addEdge(0,1);
+test.addEdge(0,2);
+test.addEdge(3,4);
+test.addEdge(3,5);
+console.log(test.pathTo(3));
