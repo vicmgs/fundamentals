c7370bd0406b5308c028b5f271ff7575510b387c
Victor Mu <vic.mgs@gmail.com>
2017-01-09 19:44:34 -0800

    Add custom directive to angular

--- UIs/angClock.js
+++ UIs/angClock.js
@@ -1,7 +1,10 @@
 'use strict';
 
-angular.module('clock', []).
-controller('clockController', function($scope, $interval) {
+angular.module('clock', [])
+.run(function($rootScope) {
+  $rootScope.example = 'just a test';
+})
+.controller('clockController', function($scope, $interval) {
   var timeNow = function() {
     var time =  new Date();
     return time.toString().slice(15, 24);
@@ -12,3 +15,34 @@ controller('clockController', function($scope, $interval) {
   }, 1000, 0);
   $scope.time = timeNow();
 })
+.controller('clockController2', function($scope, $interval) {
+  var dateNow = function() {
+    var date =  new Date();
+    return date.toString().slice(4, 10);
+  }
+
+  $interval(function(){
+    $scope.date = dateNow();
+  }, 1000, 0);
+  $scope.date = dateNow();
+})
+.directive('helloWorld', function() {
+  return {
+    scope: {
+      example: '@'
+    },
+    restrict: 'AE',
+    replace: true,
+    template: '<p>{{example}}</p>',
+    link: function(scope, elem, attrs) {
+      elem.bind('click', function() {
+        scope.$apply(function() {
+          scope.example = "white";
+        });
+      });
+      elem.bind('mouseover', function() {
+        elem.css('cursor', 'pointer');
+      });
+    }
+  };
+});
